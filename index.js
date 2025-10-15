b9282efd6879add95631b07438d756132ec1c49e
Bill Ni <bill.8ni@gmail.com>
2025-10-15 07:34:09 -0400

    fetch code in callback page

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-if (window.location.search) {
-    const params = new URLSearchParams(window.location.search);
-
-    const codeVal = params.get('code');
-
-    window.localStorage.setItem('code', codeVal);
-
-    window.location.href = 'https://bnidevs.github.io/common-ground/';
-}
-
 const data = {};
 
 const playlists = data['items'].map((e) => {
--- spotify-callback/index.js
+++ spotify-callback/index.js
@@ -1,4 +1,6 @@
 const urlParams = new URLSearchParams(window.location.search);
 let code = urlParams.get('code');
 
-window.location.href = `https://bnidevs.github.io/common-ground/?code=${code}`;
+window.localStorage.setItem('code', code);
+
+window.location.href = 'https://bnidevs.github.io/common-ground/';
